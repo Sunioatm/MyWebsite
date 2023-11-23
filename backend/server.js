df7f718ee7bf8c0a1007bdd8d5168b3e7b30c5e2
Sunioatm <104454675+Sunioatm@users.noreply.github.com>
2023-11-23 22:31:44 +0700

    change back

--- backend/server.js
+++ backend/server.js
@@ -23,7 +23,7 @@ app.use('/api/users', userRoutes);
 
 if (process.env.NODE_ENV === 'production') {
   const __dirname = path.resolve();
-  app.use(express.static('/opt/render/project/src/frontend/dist/index.html'));
+  app.use(express.static('/frontend/dist'));
 
   app.get('*', (req, res) =>
     res.sendFile(path.resolve('frontend', 'dist', 'index.html'))
